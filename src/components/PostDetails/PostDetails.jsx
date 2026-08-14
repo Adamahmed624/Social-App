07cd391a8affe9ac180586079e1c593ec7c2c6c4
Adamahmed624 <adamdmdm999@gmail.com>
2026-08-15 00:39:38 +0300

    fixing resposive design

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -23,12 +23,12 @@ export default function PostDetails() {
         queryFn: getSinglePost
     })
 
-    if (isLoading) return <div className="max-w-2/4 mx-auto mt-4">
+    if (isLoading) return <div className="max-w-2xl mx-auto mt-4 px-4">
         <p className="bg-[#171B21] border border-[#262626] px-4 py-6 text-sm text-[#5c6270] rounded-lg text-center font-bold ">Loading...</p>
     </div>
 
     return (
-        <div className="max-w-2/4 mx-auto mt-4">
+        <div className="max-w-2xl mx-auto mt-4 px-4">
             <button
                 onClick={() => navigate(-1)}
                 className="cursor-pointer inline-flex mb-4 items-center gap-2 rounded-lg bg-[#171B21] border border-[#262626] px-3 py-2 text-sm font-bold text-[#5c6270] transition-colors duration-200 hover:bg-[#1F2430] hover:border-[#333333] hover:text-[#8A90A0]">
